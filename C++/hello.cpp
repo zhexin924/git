ac8b4675da59e668732e5d841352809f64e29dee
Zhexin <jessicazhang0924@gmail.com>
2016-05-01 16:09:56 -0500

    c++

---
+++ C++/hello.cpp
@@ -0,0 +1,10 @@
+/*************************************************************************
+	> File Name: hello.cpp
+	> Author:Zhexin Zhang 
+	> Mail:jessicazhang0924@gmail.com 
+	> Created Time: Sun 01 May 2016 04:09:06 PM CDT
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+cout<<"Hello world!"<<endl;
